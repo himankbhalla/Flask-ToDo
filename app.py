3e819e013e4a9afdea6d640061a91d1e29c08189
Himank Bhalla <himank.bhalla@codingblocks.com>
2019-06-27 13:16:12 +0530

    add db , add model and request methods

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask , render_template
+from flask import Flask , render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo_database.db'
+
+# Initialize the database
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200) , nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __str__(self):
+        return 'Task: {}'.format(self.id)
 # Index route
 
-@app.route('/')
+@app.route('/', methods=['GET' , 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        return 'hello'
+    else:
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
